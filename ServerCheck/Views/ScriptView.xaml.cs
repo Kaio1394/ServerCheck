e94b6099d3af4ad1250f6bf77a2e43cf532b5b39
Kaio <kaio.santiago.13@hotmail.com>
2025-07-12 16:57:32 -0300

    increase view Script

--- ServerCheck/Views/ScriptView.xaml.cs
+++ ServerCheck/Views/ScriptView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ServerCheck.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +24,7 @@ namespace ServerCheck.Views
         public ScriptView()
         {
             InitializeComponent();
+            DataContext = new ScriptViewModel();
         }
     }
 }
